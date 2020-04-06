b4dad12df68e362679f768b3fbd2e29bf9d28099
Pawel Rabel <pawel.rabel@appunite.com>
2020-04-06 14:48:36 +0200

    Fix issue with ENV

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,5 +3,5 @@
 # Decrypt the file
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output android/app/google-services.json android/app/google-services.json.gpg
